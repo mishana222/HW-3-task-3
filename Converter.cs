3850c20289f00df9c397932f9f8e7dd389c59ad0
mishana222 <92452731+mishana222@users.noreply.github.com>
2021-10-13 18:03:42 +0300

    Add files via upload

---
+++ Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_3_task_3
+{
+	class Converter
+	{
+		public Converter(double usd, double eur)
+		{
+			Usd = usd;
+			Eur = eur;
+		}
+
+		public  double StartConvert (double amount, string CurrencyFrom, string CurrencyTo)
+		{
+			if      (CurrencyFrom == "UAH" && CurrencyTo == "USD")
+				return amount / Usd;
+			else if (CurrencyFrom == "UAH" && CurrencyTo == "EUR")
+				return amount / Eur;
+			else if (CurrencyFrom == "USD" && CurrencyTo == "UAH")
+				return amount * Usd;
+			else if (CurrencyFrom == "EUR" && CurrencyTo == "UAH")
+				return amount * Eur;
+			else if ( (CurrencyFrom == "UAH" && CurrencyTo == "UAN")
+				    || (CurrencyFrom == "USD" && CurrencyTo == "USD")
+				    || (CurrencyFrom == "EUR" && CurrencyTo == "EUR") )
+				return amount;
+
+			else return -1;
+		}
+
+		public double Usd;
+		public double Eur;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HW_3_task_3
+{
+	class Program
+	{
+		static void Set(ref double amount,ref string CurrencyFrom,ref string CurrencyTo)
+		{
+			Console.WriteLine("Enter amount of money you wish to convert:");
+			amount = Convert.ToDouble(Console.ReadLine());
+			while (amount <= 0)
+			{
+				Console.WriteLine("Inadmissible amount of money entered. Please, try again.");
+				amount = Convert.ToDouble(Console.ReadLine() );
+			}
+
+			Console.WriteLine("Enter the Currency which you want to convert from:\n" +
+				"(UAH/USD/EUR only)");
+			CurrencyFrom = Console.ReadLine();
+			while (CurrencyFrom != "UAH" && CurrencyFrom != "USD" && CurrencyFrom != "EUR")
+			{
+				Console.WriteLine("Wrong currency entered. Please, try again.");
+				CurrencyFrom = Console.ReadLine();
+			}
+
+			Console.WriteLine("Enter the Currency which you want to convert to:\n" +
+				"(UAH/USD/EUR only)");
+			CurrencyTo = Console.ReadLine();
+			while (CurrencyTo != "UAH" && CurrencyTo != "USD" && CurrencyTo != "EUR")
+			{
+				Console.WriteLine("Wrong currency entered. Please, try again.");
+				CurrencyTo = Console.ReadLine();
+			}
+		}
+		static void Main(string[] args)
+		{
+			double amount = 0;
+			string CurrencyFrom = "", CurrencyTo = "";
+
+			Set(ref amount,ref CurrencyFrom,ref CurrencyTo);
+
+			Converter work = new Converter(8, 10);
+			
+			Console.WriteLine($"Your result: {work.StartConvert(amount, CurrencyFrom, CurrencyTo)} ");
+		}
+	}
+}
